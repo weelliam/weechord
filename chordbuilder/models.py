37cdd03dc3b1ed1bad07d647b0d57aa815355efe
William Petit <w.petit@ed.ac.uk>
2016-02-25 17:40:28 +0000

    Added the notes to the DB and some html page to view the list.

--- chordbuilder/models.py
+++ chordbuilder/models.py
@@ -9,9 +9,12 @@ from django.db import models
 #    scale = models.ForeignKey(Scale)
 #    notes = models.ManyToManyField(Note)
 
-#class Scale(models.Model):
-#    name = models.CharField(max_length=200)
-#    notes = models.ManyToManyField(Note)
+class Scale(models.Model):
+    name = models.CharField(max_length=200)
+
+class Interval(models.Model):
+    name = models.CharField(max_length=200)
+    unit = models.IntegerField(default=0)
 
 class Note(models.Model):
     name = models.CharField(max_length=5)
@@ -19,7 +22,16 @@ class Note(models.Model):
     is_sharp = models.BooleanField(default=0)
 
     def __str__(self):
-        return self.name
+        if self.is_sharp:
+            return self.name + "/" + self.get_flat_equivalent()
+        else:
+            return self.name
 
     def next_note(self):
         return self.sharp
+
+    def get_flat_equivalent(self):
+        if self.is_sharp:
+            return self.next_note().name + u"\u266D"
+        else:
+            return False
--- chordbuilder/views.py
+++ chordbuilder/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Note
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def notes(request):
+    all_note_list = Note.objects.order_by('name')
+    context = {'all_note_list': all_note_list}
+
+    return render(request, 'chordbuilder/notes.html', context)
